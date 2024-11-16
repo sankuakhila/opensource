6c3ebfe5eee1b16df0a7e91b5bc1fcc1321f9ae2
sankuakhila <159749394+sankuakhila@users.noreply.github.com>
2024-11-16 20:16:43 +0530

    Create RPS.py

---
+++ visaa_prep/RPS.py
@@ -0,0 +1,15 @@
+v,c=list(map(str,input().split()))
+if(v=="P" and c=="R"):
+    print("Vignesh")
+elif(v=="S" and c=="P"):
+    print("Vignesh")
+elif(v=="R" and c=="S"):
+    print("Vignesh")
+elif(v=="R" and c=="P"):
+    print("Charan")
+elif(v=="P" and c=="S"):
+    print("Charan")
+elif(v=="S" and c=="R"):
+    print("Charan")
+else:
+    print("NULL")
